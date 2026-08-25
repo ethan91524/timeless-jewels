394894008c1566697f9bdcb0cffb8eca2bf40987
ethan91524 <ethan91524@users.noreply.github.com>
2026-08-25 04:35:51 +0000

    deploy: fe9224641df2bc6c8162873cdec0470e546c7a4d

---
+++ _app/immutable/chunks/paths.638506f5.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1yhc13v)==null?void 0:s.base)??"/timeless-jewels";var e;const a=((e=globalThis.__sveltekit_1yhc13v)==null?void 0:e.assets)??t;export{a,t as b};
